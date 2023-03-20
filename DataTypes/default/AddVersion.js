81ee7aa86e004bf7487089417a81a2f5dea01c37
shaunaksangdod <shaunaksangdod@gmail.com>
2023-03-20 16:54:26 -0400

    restructure

--- DataTypes/default/AddVersion.js
+++ DataTypes/default/AddVersion.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import get from "lodash.get";
 import { DataTypes } from "../index";
-import { datasets } from "../open_fema_data/create";
 
 const AddVersion = ({ source, views, user, baseUrl }) => {
   console.log('???', source, views, user)
@@ -12,10 +11,6 @@ const AddVersion = ({ source, views, user, baseUrl }) => {
     console.log(source)
       let sourceTypeToFileNameMapping = get(source, 'type', '').substring(0, 3) === "tl_" ? "tiger_2017" : source.type;
 
-      if (datasets.includes(sourceTypeToFileNameMapping)){
-        //need to crete separate data types
-        sourceTypeToFileNameMapping = 'open_fema_data'
-      }
       return get(DataTypes, `[${sourceTypeToFileNameMapping}].sourceCreate.component`, () => <div />)
     }, [source]);
 
--- DataTypes/index.js
+++ DataTypes/index.js
@@ -14,19 +14,24 @@ import npmrds_tmc_identification_imp from "./npmrds/npmrds_tmc_identification_im
 import npmrds_tmc_identification from "./npmrds/npmrds_tmc_identification";
 
 // hazmit types
-import ncei_storm_events from './ncei_storm_events';
-import ncei_storm_events_enhanced from "./ncei_storm_events_enhanced";
-import zone_to_county from "./zone_to_county";
-import tiger_2017 from "./tiger_2017";
-import open_fema_data from "./open_fema_data";
-import usda_crop_insurance_cause_of_loss from "./usda";
-import sba_disaster_loan_data_new from "./sba";
-import nri from "./nri";
-import per_basis from "./per_basis_swd";
-import hlr from "./hlr";
-import eal from "./eal"
-import disaster_loss_summary from "./disaster_loss_summary";
-import fusion from "./fusion";
+import disaster_declarations_summaries_v2 from "./hazard_mitigation/disaster_declarations_summaries_v2";
+import fema_nfip_claims_v1 from "./hazard_mitigation/fema_nfip_claims_v1";
+import individuals_and_households_program_valid_registrations_v1
+  from "./hazard_mitigation/individuals_and_households_program_valid_registrations_v1";
+import public_assistance_funded_projects_details_v1
+  from "./hazard_mitigation/public_assistance_funded_projects_details_v1";
+import ncei_storm_events from './hazard_mitigation/ncei_storm_events';
+import ncei_storm_events_enhanced from "./hazard_mitigation/ncei_storm_events_enhanced";
+import zone_to_county from "./hazard_mitigation/zone_to_county";
+import tiger_2017 from "./hazard_mitigation/tiger_2017";
+import usda_crop_insurance_cause_of_loss from "./hazard_mitigation/usda";
+import sba_disaster_loan_data_new from "./hazard_mitigation/sba";
+import nri from "./hazard_mitigation/nri";
+import per_basis from "./hazard_mitigation/per_basis_swd";
+import hlr from "./hazard_mitigation/hlr";
+import eal from "./hazard_mitigation/eal"
+import disaster_loss_summary from "./hazard_mitigation/disaster_loss_summary";
+import fusion from "./hazard_mitigation/fusion";
 
 const DataTypes = {
   freight_atlas_shapefile,
@@ -44,19 +49,31 @@ const DataTypes = {
   npmrds_tmc_identification_imp,
   npmrds_tmc_identification,
 
-  // hazmit types
-  ncei_storm_events,
-  ncei_storm_events_enhanced,
+  // hazmit types: geo
   zone_to_county,
   tiger_2017,
-  open_fema_data,
+
+  // hazmit types: swd
+  ncei_storm_events,
+  ncei_storm_events_enhanced,
+
+  // hazmit types: other data
   usda_crop_insurance_cause_of_loss,
   sba_disaster_loan_data_new,
   nri,
+
+  // hazmit types: open fema data types
+  disaster_declarations_summaries_v2,
+  fema_nfip_claims_v1,
+  individuals_and_households_program_valid_registrations_v1,
+  public_assistance_funded_projects_details_v1,
+
+  ofd: disaster_loss_summary,
+
+  // hazmit types: AVAIL processing
   per_basis,
   hlr,
   eal,
-  ofd: disaster_loss_summary,
   fusion
 };
 
