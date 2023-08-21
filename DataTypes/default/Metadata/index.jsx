0b31d34930ef363535f7a25c34d7faf915a8020a
alex <am3081@gmail.com>
2023-08-21 14:24:15 -0400

    no logs

--- DataTypes/default/Metadata/index.jsx
+++ DataTypes/default/Metadata/index.jsx
@@ -4,7 +4,7 @@ import {MetadataTable} from "./components/MetadataTable.jsx";
 const Index = ({source, views, ...props}) => {
     return (
         <div className="w-full flex-1 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <div className='col-span-3'>zz
+            <div className='col-span-3'>
                 <MetadataTable source={source}/>
             </div>
         </div>
