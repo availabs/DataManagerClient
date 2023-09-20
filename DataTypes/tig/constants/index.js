361514910d3c7915b76b50a2db55feeffa411277
sarangp3010 <spatel25@albany.edu>
2023-09-20 15:46:11 -0400

    Acs chart added

--- DataTypes/tig/constants/index.js
+++ DataTypes/tig/constants/index.js
@@ -44,7 +44,7 @@ const regionalData = {
     ],
   },
   sub_regions: {
-    "Connecticut": ["Fairfield", "Litchfield", "New Haven"],
+    Connecticut: ["Fairfield", "Litchfield", "New Haven"],
     "Long Island": ["Nassau", "Suffolk"],
     "Mid Hudson": [
       "Dutchess",
@@ -76,4 +76,39 @@ const regionalData = {
   },
 };
 
-export { regionalData };
+const fips2Name = {
+  "34003": "Bergen",
+  "34013": "Essex",
+  "34017": "Hudson",
+  "34019": "Hunterdon",
+  "34021": "Mercer",
+  "34023": "Middlesex",
+  "34025": "Monmouth",
+  "34027": "Morris",
+  "34029": "Ocean",
+  "34031": "Passaic",
+  "34035": "Somerset",
+  "34037": "Sussex",
+  "34039": "Union",
+  "34041": "Warren",
+  "36005": "Bronx",
+  "36027": "Dutchess",
+  "36031": "Essex",
+  "36047": "Kings",
+  "36059": "Nassau",
+  "36061": "New York",
+  "36071": "Orange",
+  "36079": "Putnam",
+  "36081": "Queens",
+  "36085": "Richmond",
+  "36087": "Rockland",
+  "36103": "Suffolk",
+  "36113": "Warren",
+  "36119": "Westchester",
+  "09001": "Fairfield",
+  "09005": "Litchfield",
+  "09007": "Middlesex",
+  "09009": "New Haven",
+};
+
+export { regionalData, fips2Name };
