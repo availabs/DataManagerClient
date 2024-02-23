aceacd807d590c0c098038a6c2c89167cdd7a3a0
alex <am3081@gmail.com>
2024-02-23 13:47:18 -0500

    stuff

--- Collection/CollectionTypes/default/index.jsx
+++ Collection/CollectionTypes/default/index.jsx
@@ -1,9 +1,5 @@
 import Overview from "./Overview";
-import MetadataAdvanced from "./Metadata/advanced.jsx";
-import MetadataBasic from "./Metadata/basic.jsx";
 import Versions from "./Version/list";
-import AddVersion from "./Version/add";
-import Uploads from "./Uploads";
 import Admin from "./Admin"
 import SymbologyEditor from "./Symbology"
 import MapEditor from "./MapEditor"
@@ -31,32 +27,6 @@ const Pages = {
     path: "/versions",
     hidden: "true",
     component: Versions
-  },
-  uploads: {
-    name: "Uploads",
-    path: "/uploads",
-    authLevel: 5,
-    hidden: true,
-    component: Uploads,
-  },
-  add_version: {
-    name: "Add Version",
-    path: "/add_version",
-    authLevel: 5,
-    hidden: true,
-    component: AddVersion
-  },
-  meta_advanced: {
-      name: "Metadata",
-      path: "/meta_advanced",
-      hidden: true,
-      component: MetadataAdvanced,
-  },
-  meta: {
-    name: "Metadata",
-    path: "/meta",
-    hidden: true,
-    component: MetadataBasic,
   },
   admin: {
     name: "Admin",
