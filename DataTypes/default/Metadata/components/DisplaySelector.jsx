473891fb7a97b0df1379d52a50af3d860e465317
shaunaksangdod <shaunaksangdod@gmail.com>
2024-03-14 14:07:59 -0400

    minor improvements

--- DataTypes/default/Metadata/components/DisplaySelector.jsx
+++ DataTypes/default/Metadata/components/DisplaySelector.jsx
@@ -9,26 +9,28 @@ export const DisplaySelector = ({sourceId, metadata, setMetadata, col, value}) =
         await editMetadata({sourceId, pgEnv, falcor, metadata, setMetadata, col, value: {display: e.target.value}});
     }, [col, metadata]);
 
-    return (<select
-        className="appearance-auto pl-3 pr-4 py-2.5 border border-blue-100 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
-        value={value}
-        onChange={onChange}
-        // disabled={value === 'calculated-column'}
-    >
-        <option value={""}>
-            none
-        </option>
-        <option value="meta-variable">
-            meta variable
-        </option>
-        <option value="data-variable">
-            data variable
-        </option>
-        <option value="geoid-variable">
-            fips variable
-        </option>
-        <option value="geom-variable">
-            geom variable
-        </option>
-    </select>)
+    return (<div className={'border border-blue-100'}>
+        <select
+            className="appearance-auto pl-3 pr-4 py-2.5 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
+            value={value}
+            onChange={onChange}
+            // disabled={value === 'calculated-column'}
+        >
+            <option value={""}>
+                none
+            </option>
+            <option value="meta-variable">
+                meta variable
+            </option>
+            <option value="data-variable">
+                data variable
+            </option>
+            <option value="geoid-variable">
+                fips variable
+            </option>
+            <option value="geom-variable">
+                geom variable
+            </option>
+        </select>
+    </div>)
 }
--- DataTypes/default/Metadata/components/TypeSelector.jsx
+++ DataTypes/default/Metadata/components/TypeSelector.jsx
@@ -9,28 +9,30 @@ export const TypeSelector = ({sourceId, metadata, setMetadata, col, value}) => {
         await editMetadata({sourceId, pgEnv, falcor, metadata, setMetadata, col, value: {type: e.target.value}});
     }, [col, metadata]);
 
-    return (<select
-        className="pl-3 pr-4 py-2.5 h-fit border border-blue-100 bg-blue-50 w-full bg-white mr-2 flex items-center justify-between text-sm"
-        value={value}
-        onChange={onChange}
-    >
-        <option value={null}>
-            none
-        </option>
-        <option value="integer">
-            integer
-        </option>
-        <option value="number">
-            number
-        </option>
-        <option value="string">
-            string
-        </option>
-        <option value="boolean">
-            boolean
-        </option>
-        <option value="object">
-            object
-        </option>
-    </select>)
+    return (<div className={'border border-blue-100'}>
+        <select
+            className="appearance-auto pl-3 pr-4 py-2.5 h-fit w-full bg-white mr-2 text-sm"
+            value={value}
+            onChange={onChange}
+        >
+            <option value={null}>
+                none
+            </option>
+            <option value="integer">
+                integer
+            </option>
+            <option value="number">
+                number
+            </option>
+            <option value="string">
+                string
+            </option>
+            <option value="boolean">
+                boolean
+            </option>
+            <option value="object">
+                object
+            </option>
+        </select>
+    </div>)
 }
