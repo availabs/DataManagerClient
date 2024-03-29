f7943a368af9f8f06b24c131cde10d9602359ae1
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2024-03-29 10:55:31 -0400

    Use `time ago` format for tasks list

--- Tasks/index.jsx
+++ Tasks/index.jsx
@@ -19,22 +19,31 @@ const ETL_CONTEXT_ATTRS = [
   "type"
 ];
 
+function timeAgo(input) {
+  const date = (input instanceof Date) ? input : new Date(input);
+  const formatter = new Intl.RelativeTimeFormat('en');
+  const ranges = {
+    years: 3600 * 24 * 365,
+    months: 3600 * 24 * 30,
+    weeks: 3600 * 24 * 7,
+    days: 3600 * 24,
+    hours: 3600,
+    minutes: 60,
+    seconds: 1
+  };
+  const secondsElapsed = (date.getTime() - Date.now()) / 1000;
+  for (let key in ranges) {
+    if (ranges[key] < Math.abs(secondsElapsed)) {
+      const delta = secondsElapsed / ranges[key];
+      return formatter.format(Math.round(delta), key);
+    }
+  }
+}
+
 const StartedAtCell = (d) => {
   const { value } = d;
-  const now = moment(new Date());
-  const cellMoment = moment(value);
-  const displayTimeThreshold = moment( now.subtract(14, 'days'));
-
-  let formattedDate; 
-  if( cellMoment.isAfter(displayTimeThreshold) ){
-    formattedDate = moment(value).format("DD MMMM hh:mm a");
-  }
-  else{
-    formattedDate = moment(value).format("MMMM Do YYYY");
-  }
-
   return (
-    <div>{ formattedDate }</div>
+    <div>{ timeAgo(value) }</div>
   )
 }
 
