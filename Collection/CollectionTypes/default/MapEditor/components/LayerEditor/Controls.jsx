d09490438b0cffb88c9c7e46ef11dc236aab7d26
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2024-05-24 14:21:29 -0400

    I think it does custom data categories pretty good

--- Collection/CollectionTypes/default/MapEditor/components/LayerEditor/Controls.jsx
+++ Collection/CollectionTypes/default/MapEditor/components/LayerEditor/Controls.jsx
@@ -49,13 +49,14 @@ export function SelectTypeControl({path, datapath, params={}}) {
   const { state, setState } = React.useContext(SymbologyContext);
   const { falcor, falcorCache, pgEnv } = React.useContext(DamaContext);
   // console.log('select control', params)
-  let { value, viewId, sourceId,paintValue, column, categorydata, choroplethdata, colors, colorrange, numCategories, numbins, method, showOther } = useMemo(() => {
+  let { value, viewId, sourceId,paintValue, column, categories, categorydata, choroplethdata, colors, colorrange, numCategories, numbins, method, showOther } = useMemo(() => {
     return {
       value: get(state, `symbology.layers[${state.symbology.activeLayer}].${path}`, {}),
       viewId: get(state,`symbology.layers[${state.symbology.activeLayer}].view_id`),
       sourceId: get(state,`symbology.layers[${state.symbology.activeLayer}].source_id`),
       paintValue : get(state, `symbology.layers[${state.symbology.activeLayer}].${datapath}`, {}),
       column: get(state, `symbology.layers[${state.symbology.activeLayer}]['data-column']`, ''),
+      categories: get(state, `symbology.layers[${state.symbology.activeLayer}]['categories']`, {}),
       categorydata: get(state, `symbology.layers[${state.symbology.activeLayer}]['category-data']`, {}),
       choroplethdata: get(state, `symbology.layers[${state.symbology.activeLayer}]['choropleth-data']`, {}),
       colors: get(state, `symbology.layers[${state.symbology.activeLayer}]['color-set']`, categoricalColors['cat1']),
@@ -109,11 +110,11 @@ export function SelectTypeControl({path, datapath, params={}}) {
 
   React.useEffect(() => {
     if( value === 'categories') {
-      let {paint, legend} = categoryPaint(column,categorydata,colors,numCategories, showOther)
+      let {paint, legend} = categories?.paint ? categories : categoryPaint(column,categorydata,colors,numCategories, showOther);
       //console.log('categories xyz', paint, isValidCategoryPaint(paint))
       if(isValidCategoryPaint(paint) && !isEqual(paint,paintValue)) {
         //console.log('update category paint', column, numCategories, showOther, categorydata, categoryPaint(column,categorydata,colors,numCategories,showOther))
-      
+
         setState(draft => {
           set(draft, `symbology.layers[${state.symbology.activeLayer}].${datapath}`, paint)
           set(draft, `symbology.layers[${state.symbology.activeLayer}]['legend-data']`, legend)
@@ -490,7 +491,6 @@ function CategoricalColorControl({path, params={}}) {
   let value = get(state, `symbology.layers[${state.symbology.activeLayer}].${path}`, colors['cat1'])
 
   // console.log('value', value, path)
-
   return (
       <div className='flex w-full items-center'>
         <ControlMenu 
@@ -533,18 +533,18 @@ function CategoryControl({path, params={}}) {
   const { falcor, falcorCache, pgEnv } = React.useContext(DamaContext);
   // console.log('select control', params)
   //let colors = categoricalColors
+
+  //Value is literal represetnation of mapbox filter object that colors the actual map
   let { value, column, categorydata, colors } = useMemo(() => {
     return {
       value: get(state, `symbology.layers[${state.symbology.activeLayer}].${path}`, {}),
       column: get(state, `symbology.layers[${state.symbology.activeLayer}]['data-column']`, ''),
       categorydata: get(state, `symbology.layers[${state.symbology.activeLayer}]['category-data']`, {}),
-      colors: get(state, `symbology.layers[${state.symbology.activeLayer}]['color-set']`, categoricalColors['cat1'])
+      colors: get(state, `symbology.layers[${state.symbology.activeLayer}]['color-set']`, categoricalColors['cat1']),
     }
   },[state])
-  console.log("value", value)
-  console.log("categorydata", categorydata)
 
-  //Number of distinct values, not counting `null`
+  //Number of total distinct values, not counting `null`
   const numCategories = useMemo(() => {
       //console.log('categorydata', categorydata)
       return Object.values(categorydata)
@@ -555,8 +555,8 @@ function CategoryControl({path, params={}}) {
           return out
         },0)
    }, [categorydata])
-  console.log("numCategories",numCategories)
-  //Represents the "active list" of categories
+
+  //A human readable representation of the current data categories
   const currentCategories = (Array.isArray(value) ? value : []).filter((d,i) => i > 2 )
             .map((d,i) => {
               if(i%2 === 0) {
@@ -565,9 +565,6 @@ function CategoryControl({path, params={}}) {
               return null
             })
             .filter(d => d)
-  console.log("categories",currentCategories);
-
-
 
   const availableCategories = getDiffColumns(
     Object.values(categorydata)
@@ -575,7 +572,7 @@ function CategoryControl({path, params={}}) {
       .map((catData) => catData[column]),
     currentCategories.map((cat) => cat.label)
   ).map((cat) => ({ label: cat, value: cat }));
-  console.log("availableCategories",availableCategories)
+
   const showOther = get(state, `symbology.layers[${state.symbology.activeLayer}]['category-show-other']`,'#ccc') === '#ccc'
   return (
    
@@ -587,8 +584,9 @@ function CategoryControl({path, params={}}) {
               className='w-full p-2 bg-transparent text-slate-700 text-sm'
               value={currentCategories.length}
               onChange={(e) => setState(draft => {
-                // console.log('SelectViewColumnControl set column path', path, e.target.value)
-                set(draft, `symbology.layers[${state.symbology.activeLayer}].['num-categories']`, e.target.value)
+                  set(draft, `symbology.layers[${state.symbology.activeLayer}].['num-categories']`, e.target.value);
+                  set(draft, `symbology.layers[${state.symbology.activeLayer}].categories`,{
+                });
               })}
             >
               <option key={'def'} value={currentCategories.length}>{currentCategories.length} Categories</option>
@@ -632,11 +630,32 @@ function CategoryControl({path, params={}}) {
         </div>
         <div className='w-full max-h-[250px] overflow-auto'>
         {currentCategories.map((d,i) => (
-          <div key={i} className='w-full flex items-center hover:bg-slate-100'>
+          <div key={i} className='group/title w-full flex items-center hover:bg-slate-100'>
             <div className='flex items-center h-8 w-8 justify-center  border-r border-b '>
               <div className='w-4 h-4 rounded border-[0.5px] border-slate-600' style={{backgroundColor:d.color}}/>
             </div>
             <div className='flex items-center text-center flex-1 px-4 text-slate-600 border-b h-8 truncate'>{d.label}</div>
+            <div
+              className="group/icon border-b w-8 h-8 flex items-center border-slate-200 cursor-pointer fill-white group-hover/title:fill-slate-300 hover:bg-slate-200"
+              onClick={() => {
+                const updatedCategoryPaint = [...value];
+                const updatedCategoryLegend = currentCategories.filter(cat => cat.label !== d.label);
+
+                const indexOfLabel = updatedCategoryPaint.indexOf(d.label);
+
+                //In filter array, the `label` preceeds its paint `value`
+                updatedCategoryPaint.splice(indexOfLabel, 2);
+                setState(draft=> {
+                  set(draft, `symbology.layers[${state.symbology.activeLayer}].categories`,{
+                    paint: updatedCategoryPaint, legend: updatedCategoryLegend
+                  });
+                });
+              }}
+            >
+              <Close
+                className="mx-[6px] cursor-pointer group-hover/icon:fill-slate-500 "
+              />
+            </div>
           </div> 
         ))}
         {showOther && <div className='w-full flex items-center hover:bg-slate-100'>
@@ -648,13 +667,29 @@ function CategoryControl({path, params={}}) {
         }
         <div className="flex-1 flex items-center mx-4">
           <StyledControl>
-          <label className='flex w-full'>
+            <label className='flex w-full'>
               <div className='flex w-full items-center'>
                 <select
                   className='w-full py-2 bg-transparent'
                   value={''}
                   onChange={(e) =>
-                    {console.log("adding new value to categoires", e)}
+                    {
+                      const updatedCategoryPaint = [...value];
+                      const updatedCategoryLegend = [...currentCategories];
+
+                      const lastColorUsed = updatedCategoryPaint[updatedCategoryPaint.length-2];
+                      const lastColorIndex = colors.map(color => rgb2hex(color)).indexOf(lastColorUsed);
+                      const nextColor = lastColorIndex < colors.length-1 ? colors[lastColorIndex+1] : colors[0];
+
+                      updatedCategoryLegend.push({ color: nextColor, label: e.target.value })
+                      updatedCategoryPaint.splice(value.length-1, 0, e.target.value, rgb2hex(nextColor));
+
+                      setState(draft=> {
+                        set(draft, `symbology.layers[${state.symbology.activeLayer}].categories`,{
+                          paint: updatedCategoryPaint, legend: updatedCategoryLegend
+                        });
+                      });
+                    }
                   }
                 >
                   <option key={-1} value={""}></option>
