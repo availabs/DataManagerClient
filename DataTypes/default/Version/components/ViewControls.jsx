6482af8efcc50eea11c9df8486950e2e36cf68b2
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2024-05-30 15:12:42 -0400

    use large modal for create download

--- DataTypes/default/Version/components/ViewControls.jsx
+++ DataTypes/default/Version/components/ViewControls.jsx
@@ -289,6 +289,7 @@ export function ViewControls ({view}) {
             <Modal
                 open={modalState.open}
                 setOpen={setModalOpen}
+                themeOptions={{size:"large"}}
             >
                 <div className="flex items-center">
                     <div
