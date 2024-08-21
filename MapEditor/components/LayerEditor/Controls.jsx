5423de775ade05bb12b9f6ec15c730a018b237e5
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2024-08-21 13:23:50 -0400

    Move interactive filter component to its own directory
    also fix being able to nest interactive filters

--- MapEditor/components/LayerEditor/Controls.jsx
+++ MapEditor/components/LayerEditor/Controls.jsx
@@ -13,7 +13,7 @@ import get from 'lodash/get'
 import set from 'lodash/set'
 import cloneDeep from 'lodash/cloneDeep'
 import { CategoryControl } from './CategoryControl';
-import StyleEditor from './StyleEditor';
+import { InteractiveFilterControl } from './InteractiveFilterControl';
 
 function ControlMenu({ button, children}) {
   const { state, setState  } = React.useContext(SymbologyContext);
@@ -107,8 +107,7 @@ export function SelectTypeControl({path, datapath, params={}}) {
       {name:'Simple', value: 'simple'},
       hasCols ? {name:'Categories', value: 'categories'} : null,
       hasNumber ? {name:'Color Range', value: 'choropleth'} : null,
-      //TODO filter out interactive here, when its the sub-menu
-      {name:'Interactive', value: 'interactive'}
+      params?.version !== "interactive" ? {name:'Interactive', value: 'interactive'} : null
     ].filter(d => d)
   },[metadata])
 
@@ -811,152 +810,6 @@ function ChoroplethControl({path, params={}}) {
     )
 }
 
-function InteractiveFilterControl({ path, params = {} }) {
-  const { state, setState } = React.useContext(SymbologyContext);
-  const { value: interactiveFilters, selectedInteractiveFilterIndex, layerName, activeLayer } = useMemo(() => {
-    return {
-      value: get(
-        state,
-        `symbology.layers[${state.symbology.activeLayer}].${path}`,
-        []
-      ),
-      selectedInteractiveFilterIndex: get(
-        state,
-        `symbology.layers[${state.symbology.activeLayer}]['selectedInteractiveFilterIndex']`
-      ),
-      layerName: get(
-        state,
-        `symbology.layers[${state.symbology.activeLayer}]['name']`,
-        ''
-      ),
-      activeLayer: get(state,`symbology.layers[${state?.symbology?.activeLayer}]`, {})
-    };
-  }, [state]);
-
-  useEffect(() => {
-    if(selectedInteractiveFilterIndex !== undefined && !interactiveFilters[selectedInteractiveFilterIndex]){
-      setState(draft => {
-        set(draft, `symbology.layers[${state.symbology.activeLayer}]['selectedInteractiveFilterIndex']`, undefined)
-      })
-    }
-  }, [interactiveFilters])
-
-  const shouldDisplayInteractiveBuilder = selectedInteractiveFilterIndex !== undefined && selectedInteractiveFilterIndex !== null;
-  return (
-    <div className=" w-full items-center">
-      <Button
-        themeOptions={{ size: "xs", color: 'primary' }}
-        className={"col-span-2 capitalize mb-2"}
-        onClick={() => {
-          setState(draft => {
-            //TODO lookin ok here, still a little dicey
-            //TODO fix issue with default colors when adding new simple layer, its using old color data
-            const newInteractiveFilter = {
-              ...activeLayer,
-              "layer-type": 'simple',
-              "label": `${layerName}`,
-              selectedInteractiveFilterIndex: undefined
-            }
-
-            set(draft,`symbology.layers[${state.symbology.activeLayer}].${path}`, [...interactiveFilters, newInteractiveFilter] )
-          })
-
-        }}
-      >
-        Add interactive filter
-      </Button>
-      {
-        interactiveFilters.map((iFilter,i) => {
-          const isSelectedFilter = selectedInteractiveFilterIndex === i;
-          //TODO some small style changes -- if a certain filter is selected, the `hover` effect is not noticeable
-          return (
-            <div
-              key={`ifilter_row_${i}`}
-              className={`group/title w-full text-sm grid grid-cols-12 items-center rounded mb-2 ${isSelectedFilter && 'bg-blue-100'}`}
-            >
-              <div
-                className="truncate col-span-1 flex justify-center items-center"
-              >
-                <input
-                  type="radio"
-                  checked={isSelectedFilter}
-                  onChange={() => {
-                    setState(draft => {
-                      set(draft, `symbology.layers[${state.symbology.activeLayer}]['selectedInteractiveFilterIndex']`, i)
-                    })
-                  }}
-                />
-              </div>  
-              <div className="truncate col-span-10">
-                <input
-                  type="text"
-                  className=" px-2  border text-sm border-transparent hover:border-slate-200 outline-2 outline-transparent rounded-md bg-transparent text-slate-700 placeholder:text-gray-400 focus:outline-pink-300 sm:leading-6"
-                  value={iFilter.label}
-                  onChange={(e) => {
-                    setState(draft => {
-                      set(draft,`symbology.layers[${state.symbology.activeLayer}].${path}[${i}]['label']`, e.target.value )
-                    })
-                  }}
-                />
-              </div>
-              <div
-                className="col-span-1 flex items-center cursor-pointer group-hover/title:fill-slate-700 hover:bg-slate-100 rounded group/icon p-0.5"
-                onClick={() => {
-                  setState(draft => {
-                    const oldInteractiveFilters = get(
-                      draft,
-                      `symbology.layers[${state.symbology.activeLayer}].${path}`
-                    )
-                    oldInteractiveFilters.splice(i,1);
-                    set(draft,`symbology.layers[${state.symbology.activeLayer}].${path}`, oldInteractiveFilters )
-                  })
-                }}
-              >
-                <Close
-                  size={20}
-                  className="m-0.5 cursor-pointer group-hover/icon:fill-slate-900 "
-                />
-              </div>
-            </div>
-          )
-        })
-      }
-      {
-        shouldDisplayInteractiveBuilder && <InteractiveFilterbuilder />
-      }
-    </div>
-  );
-}
-
-export const InteractiveFilterbuilder = () => {
-  const { state, setState } = React.useContext(SymbologyContext);
-  const { interactiveFilters, selectedInteractiveFilterIndex } = useMemo(() => {
-    return {
-      interactiveFilters: get(
-        state,
-        `symbology.layers[${state.symbology.activeLayer}]['interactive-filters']`,
-        []
-      ),
-      selectedInteractiveFilterIndex: get(
-        state,
-        `symbology.layers[${state.symbology.activeLayer}]['selectedInteractiveFilterIndex']`,
-        []
-      ),
-    };
-  }, [state]);
-  return (
-    <>
-      Editing: <b>{interactiveFilters[selectedInteractiveFilterIndex]?.label}</b>
-      <StyleEditor
-        type={"interactive"}
-        pathPrefix={`['interactive-filters'][${selectedInteractiveFilterIndex}]`}
-      />
-    </>
-  );
-};
-
-
-//RYAN TODO -- make sure this doesn't blow up somehow with new Interactive Filters
 export const AddColumnSelectControl = ({setState, availableColumnNames}) => {
   return (
     <>
