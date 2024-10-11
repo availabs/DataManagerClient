66be5c2ddafdc472c6b14ec661fe4705702f3f1b
Alex Muro <am3081@gmail.com>
2024-10-11 10:30:53 -0400

    npmrds out of datamanager

--- DataTypes/index.js
+++ DataTypes/index.js
@@ -5,14 +5,10 @@ import Pages from "./default";
 // ---------------------------
 import gis_dataset from "./gis_dataset";
 import csv_dataset from "./csv_dataset";
-import npmrds from "./npmrds";
-import npmrds_meta from "./npmrds_meta";
 
 const damaDataTypes = {
   gis_dataset,
-  csv_dataset,
-  npmrds,
-  npmrds_meta
+  csv_dataset
 }
 
 function registerDataType (name, dataType) {
