d768e66eb2385b49c9490ce73f0325f715377eb9
sarangp3010 <spatel25@albany.edu>
2025-07-31 12:46:17 -0400

    added analytics stuff

--- DataTypes/index.js
+++ DataTypes/index.js
@@ -5,10 +5,12 @@ import Pages from "./default";
 // ---------------------------
 import gis_dataset from "./gis_dataset";
 import csv_dataset from "./csv_dataset";
+import analytics from "./analytics";
 
 const damaDataTypes = {
   gis_dataset,
-  csv_dataset
+  csv_dataset,
+  analytics
 }
 
 function registerDataType (name, dataType) {
