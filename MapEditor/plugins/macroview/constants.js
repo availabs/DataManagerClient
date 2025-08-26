8e3682574084c105ac946517feb6f980df9512bc
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2025-08-26 09:53:42 -0400

    fix small bug with geom layers, display region name instead of code

---
+++ MapEditor/plugins/macroview/constants.js
@@ -0,0 +1,15 @@
+const REGION_CODE_TO_NAME = {
+  1: "Region 1 - Capital District",
+  2: "Region 2 - Mohawk Valley",
+  3: "Region 3 - Central New York",
+  4: "Region 4 - Genesee Valley",
+  5: "Region 5 - Western New York",
+  6: "Region 6 - Southern Tier/Central New York",
+  7: "Region 7 - North Country",
+  8: "Region 8 - Hudson Valley",
+  9: "Region 9 - Southern Tier",
+  10: "Region 10 - Long Island",
+  11: "Region 11 - New York City",
+};
+
+export { REGION_CODE_TO_NAME };
